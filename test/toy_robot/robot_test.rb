b6a0e6412974485c5481440415792b5855c2ec88
Sam Wilson <sam.wilson@sharesight.co.nz>
2021-09-24 11:18:27 +1200

    Robot can move in the direction it is facing

--- test/toy_robot/robot_test.rb
+++ test/toy_robot/robot_test.rb
@@ -4,55 +4,105 @@ require 'minitest/autorun'
 require 'test_helper'
 
 describe ToyRobot::Robot do
-  before do
-    @subject = ToyRobot::Robot.new(0)
+  describe 'moving in a direction' do
+    before do
+      @subject = ToyRobot::Robot.new(0)
+    end
+
+    it 'moves 3 spaces east' do
+      3.times { @subject.move_east }
+
+      _(@subject.east).must_equal(3)
+    end
+
+    it 'moves 4 spaces east' do
+      4.times { @subject.move_east }
+
+      _(@subject.east).must_equal(4)
+    end
+
+    it 'moves 3 spaces west' do
+      3.times { @subject.move_west }
+
+      _(@subject.east).must_equal(-3)
+    end
+
+    it 'moves 4 spaces west' do
+      4.times { @subject.move_west }
+
+      _(@subject.east).must_equal(-4)
+    end
+
+    it 'moves 3 spaces north' do
+      3.times { @subject.move_north }
+
+      _(@subject.north).must_equal(3)
+    end
+
+    it 'moves 4 spaces north' do
+      4.times { @subject.move_north }
+
+      _(@subject.north).must_equal(4)
+    end
+
+    it 'moves 3 spaces south' do
+      3.times { @subject.move_south }
+
+      _(@subject.north).must_equal(-3)
+    end
+
+    it 'moves 3 spaces south' do
+      3.times { @subject.move_south }
+
+      _(@subject.north).must_equal(-3)
+    end
   end
 
-  it 'moves 3 spaces east' do
-    3.times { @subject.move_east }
+  describe 'when facing north' do
+    before do
+      @subject = ToyRobot::Robot.new(0, 0, 'NORTH')
+    end
 
-    _(@subject.east).must_equal(3)
+    it 'moves north' do
+      @subject.move
+
+      _(@subject.north).must_equal(1)
+    end
   end
 
-  it 'moves 4 spaces east' do
-    4.times { @subject.move_east }
+  describe 'when facing south' do
+    before do
+      @subject = ToyRobot::Robot.new(0, 0, 'SOUTH')
+    end
 
-    _(@subject.east).must_equal(4)
+    it 'moves south' do
+      @subject.move
+
+      _(@subject.north).must_equal(-1)
+    end
   end
 
-  it 'moves 3 spaces west' do
-    3.times { @subject.move_west }
+  describe 'when facing east' do
+    before do
+      @subject = ToyRobot::Robot.new(0, 0, 'EAST')
+    end
 
-    _(@subject.east).must_equal(-3)
+    it 'moves east' do
+      @subject.move
+
+      _(@subject.east).must_equal(1)
+    end
   end
 
-  it 'moves 4 spaces west' do
-    4.times { @subject.move_west }
+  describe 'when facing west' do
+    before do
+      @subject = ToyRobot::Robot.new(0, 0, 'WEST')
+    end
 
-    _(@subject.east).must_equal(-4)
-  end
+    it 'moves west' do
+      @subject.move
 
-  it 'moves 3 spaces north' do
-    3.times { @subject.move_north }
-
-    _(@subject.north).must_equal(3)
-  end
-
-  it 'moves 4 spaces north' do
-    4.times { @subject.move_north }
-
-    _(@subject.north).must_equal(4)
-  end
-
-  it 'moves 3 spaces south' do
-    3.times { @subject.move_south }
-
-    _(@subject.north).must_equal(-3)
-  end
-
-  it 'moves 3 spaces south' do
-    3.times { @subject.move_south }
-
-    _(@subject.north).must_equal(-3)
+      _(@subject.east).must_equal(-1)
+    end
   end
 end
