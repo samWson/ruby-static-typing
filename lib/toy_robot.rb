0e2649c3d707d21e20ad1e7b0e064e659456ebaf
Sam Wilson <sam.wilson@sharesight.co.nz>
2021-10-08 10:25:09 +1300

    Robot can process commands

--- lib/toy_robot.rb
+++ lib/toy_robot.rb
@@ -5,6 +5,7 @@ require_relative 'toy_robot/version'
 require 'toy_robot/robot'
 require 'toy_robot/table'
 require 'toy_robot/simulator'
+require 'toy_robot/command'
 
 module ToyRobot
   class Error < StandardError; end
---
+++ test/toy_robot/command_test.rb
@@ -0,0 +1,57 @@
+# frozen_string_literal: true
+
+require 'minitest/autorun'
+require 'test_helper'
+
+describe ToyRobot::Command do
+  describe 'PLACE' do
+    it 'processes a PLACE command' do
+      command, *args = ToyRobot::Command.process('PLACE 1,2,NORTH')
+
+      assert_equal :place, command
+      assert_equal args, [1, 2, 'NORTH']
+    end
+
+    it 'returns :invalid for an invalid PLACE command' do
+      command = ToyRobot::Command.process('PLACE 1, 2, NORTH')
+
+      assert_equal [:invalid, 'PLACE 1, 2, NORTH'], command
+    end
+  end
+
+  describe 'MOVE' do
+    it 'processes a MOVE command' do
+      command, *args = ToyRobot::Command.process('MOVE')
+
+      assert_equal :move, command
+      assert args.empty?
+    end
+  end
+
+  describe 'LEFT' do
+    it 'processes a LEFT command' do
+      command, *args = ToyRobot::Command.process('LEFT')
+
+      assert_equal :turn_left, command
+      assert args.empty?
+    end
+  end
+
+  describe 'RIGHT' do
+    it 'processes a RIGHT command' do
+      command, *args = ToyRobot::Command.process('RIGHT')
+
+      assert_equal :turn_right, command
+      assert args.empty?
+    end
+  end
+
+  describe 'REPORT' do
+    it 'processes a REPORT command' do
+      command, *args = ToyRobot::Command.process('REPORT')
+
+      assert_equal :report, command
+      assert args.empty?
+    end
+  end
+end
